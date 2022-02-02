917207242e9c0994402c95cddfc72395f3f3ab10
shedinho20 <shedinhoshedrach@gmail.com>
2022-02-02 14:06:09 +0100

    minor changes

--- src/routes/MainRoutes.tsx
+++ src/routes/MainRoutes.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { Dashboard } from "../Pages";
-import { AppRoutes } from "./AppRoutes";
 
 export const MainRoutes = () => {
   return (
